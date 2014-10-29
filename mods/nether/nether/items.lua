e919962953cea09c2139147f3559f2af0a273a88
Ombridride <quentinbd@gmail.com>
2014-10-29 19:07:50 +0100

    fix nether fruit
    test d’un fix pour résoudre le problème avec le nether fruit

--- mods/nether/nether/items.lua
+++ mods/nether/nether/items.lua
@@ -368,7 +368,7 @@ minetest.register_node("nether:apple", {
 		end
 		local amount = math.random(4, 6)
     local p_hunger = tonumber(hud.hunger[user:get_player_name()])
-    if not p_hunger then return end
+    --if not p_hunger then return end -- test si résoud notre problème avec le nether fruit
     p_hunger = p_hunger+3
     if p_hunger > 30 then p_hunger = 30 end
     hud.hunger[user:get_player_name()] = p_hunger
