1daef126e6de2de105dc4259dc813d1b7c853115
LeMagnesium <mg.minetest@gmail.com>
2014-10-29 22:48:41 +0100

    Removing useless and buggy line

--- mods/nether/nether/items.lua
+++ mods/nether/nether/items.lua
@@ -368,7 +368,6 @@ minetest.register_node("nether:apple", {
 		end
 		local amount = math.random(4, 6)
     local p_hunger = tonumber(hud.hunger[user:get_player_name()])
-    if not p_hunger then return end
     p_hunger = p_hunger+9
     if p_hunger > 30 then p_hunger = 30 end
     hud.hunger[user:get_player_name()] = p_hunger
