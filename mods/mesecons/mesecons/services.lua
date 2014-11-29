3cd50ee8175bc37ab37d5c2111d9919a1cdace41
LeMagnesium <mg.minetest@gmail.com>
2014-11-29 18:53:06 +0100

    Update of Mesecon mod (http://github.com/Jeija/minetest-mod-mesecons)
    Signed-off-by: LeMagnesium <mg.minetest@gmail.com>

--- mods/mesecons/mesecons/services.lua
+++ mods/mesecons/mesecons/services.lua
@@ -1,43 +1,67 @@
 -- Dig and place services
 
 mesecon.on_placenode = function (pos, node)
+	mesecon.update_autoconnect(pos, node)
+
 	-- Receptors: Send on signal when active
-	if mesecon:is_receptor_on(node.name) then
-		mesecon:receptor_on(pos, mesecon:receptor_get_rules(node))
+	if mesecon.is_receptor_on(node.name) then
+		mesecon.receptor_on(pos, mesecon.receptor_get_rules(node))
 	end
 
 	-- Conductors: Send turnon signal when powered or replace by respective offstate conductor
 	-- if placed conductor is an onstate one
-	if mesecon:is_conductor(node.name) then
-		if mesecon:is_powered(pos) then
+	if mesecon.is_conductor(node.name) then
+		local sources = mesecon.is_powered(pos)
+		if sources then
 			-- also call receptor_on if itself is powered already, so that neighboring
 			-- conductors will be activated (when pushing an on-conductor with a piston)
-			mesecon:turnon (pos)
-			mesecon:receptor_on (pos, mesecon:conductor_get_rules(node))
-		elseif mesecon:is_conductor_off(node.name) then
-			minetest.swap_node(pos, {name = mesecon:get_conductor_off(node)})
+			for _, s in ipairs(sources) do
+				local rule = {x = pos.x - s.x, y = pos.y - s.y, z = pos.z - s.z}
+				mesecon.turnon(pos, rule)
+			end
+			--mesecon.receptor_on (pos, mesecon.conductor_get_rules(node))
+		elseif mesecon.is_conductor_on(node) then
+			minetest.swap_node(pos, {name = mesecon.get_conductor_off(node)})
 		end
 	end
 
 	-- Effectors: Send changesignal and activate or deactivate
-	if mesecon:is_effector(node.name) then
-		if mesecon:is_powered(pos) then
-			mesecon:changesignal(pos, node, mesecon:effector_get_rules(node), "on", 1)
-			mesecon:activate(pos, node, nil, 1)
+	if mesecon.is_effector(node.name) then
+		local powered_rules = {}
+		local unpowered_rules = {}
+
+		-- for each input rule, check if powered
+		for _, r in ipairs(mesecon.effector_get_rules(node)) do
+			local powered = mesecon.is_powered(pos, r)
+			if powered then table.insert(powered_rules, r)
+			else table.insert(unpowered_rules, r) end
+
+			local state = powered and mesecon.state.on or mesecon.state.off
+			mesecon.changesignal(pos, node, r, state, 1)
+		end
+
+		if (#powered_rules > 0) then
+			for _, r in ipairs(powered_rules) do
+				mesecon.activate(pos, node, r, 1)
+			end
 		else
-			mesecon:changesignal(pos, node, mesecon:effector_get_rules(node), "off", 1)
-			mesecon:deactivate(pos, node, nil, 1)
+			for _, r in ipairs(unpowered_rules) do
+				mesecon.deactivate(pos, node, r, 1)
+			end
 		end
 	end
 end
 
 mesecon.on_dignode = function (pos, node)
-	if mesecon:is_conductor_on(node) then
-		mesecon:receptor_off(pos, mesecon:conductor_get_rules(node))
-	elseif mesecon:is_receptor_on(node.name) then
-		mesecon:receptor_off(pos, mesecon:receptor_get_rules(node))
+	if mesecon.is_conductor_on(node) then
+		mesecon.receptor_off(pos, mesecon.conductor_get_rules(node))
+	elseif mesecon.is_receptor_on(node.name) then
+		mesecon.receptor_off(pos, mesecon.receptor_get_rules(node))
 	end
+	mesecon.queue:add_action(pos, "update_autoconnect", {node})
 end
+
+mesecon.queue:add_function("update_autoconnect", mesecon.update_autoconnect)
 
 minetest.register_on_placenode(mesecon.on_placenode)
 minetest.register_on_dignode(mesecon.on_dignode)
@@ -52,7 +76,7 @@ mesecon.do_overheat = function(pos)
 	heat = heat + 1
 	meta:set_int("heat", heat)
 
-	if heat < OVERHEAT_MAX then
+	if heat < mesecon.setting("overheat_max", 20) then
 		mesecon.queue:add_action(pos, "cooldown", {}, 1, nil, 0)
 	else
 		return true
