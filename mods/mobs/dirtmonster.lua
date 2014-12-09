b4a95f223f13275a3bdc7695209163e72973e6fa
Ombridride <quentinbd@gmail.com>
2014-12-09 14:07:44 +0100

    Update dirt monster for multiple textures

--- mods/mobs/dirtmonster.lua
+++ mods/mobs/dirtmonster.lua
@@ -7,7 +7,11 @@ mobs:register_mob("mobs:dirt_monster", {
 	collisionbox = {-0.4, -0.01, -0.4, 0.4, 1.9, 0.4},
 	visual = "mesh",
 	mesh = "mobs_stone_monster.x",
-	textures = {"mobs_dirt_monster.png"},
+	--textures = {"mobs_dirt_monster.png"},
+	available_textures = {
+		total = 1,
+		texture_1 = {"mobs_dirt_monster.png"},
+	},
 	visual_size = {x=3, y=2.6},
 	makes_footstep_sound = true,
 	view_range = 16,
