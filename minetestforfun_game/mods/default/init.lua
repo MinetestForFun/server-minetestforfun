db0aa37152008eb1fc9818378256173d6aad0ad3
LeMagnesium <mg.minetest@gmail.com>
2014-12-14 22:05:45 +0100

    Merge "Rewrite furnace"

--- minetestforfun_game/mods/default/init.lua
+++ minetestforfun_game/mods/default/init.lua
@@ -34,6 +34,7 @@ gui_suvival_form = "size[8,8.5]"..
 dofile(minetest.get_modpath("default").."/functions.lua")
 dofile(minetest.get_modpath("default").."/commands.lua")
 dofile(minetest.get_modpath("default").."/nodes.lua")
+dofile(minetest.get_modpath("default").."/furnace.lua")
 dofile(minetest.get_modpath("default").."/tools.lua")
 dofile(minetest.get_modpath("default").."/craftitems.lua")
 dofile(minetest.get_modpath("default").."/crafting.lua")
