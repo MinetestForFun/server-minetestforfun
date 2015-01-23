cc128f57134c0d5867bf974c22af5264feb98a6c
LeMagnesium <mg.minetest@gmail.com>
2015-01-23 18:18:27 +0100

    Fixed debug line for NSPass in irc mod

--- mods/irc/init.lua
+++ mods/irc/init.lua
@@ -105,7 +105,7 @@ function irc:connect()
 	end
 
 	print("== This is a debug line, please check for it ==")
-	print(self.config.NSPASS)
+	print(self.config.NSPass)
 	print("=== DEBUG FINISHED ===")
 	if self.config.NSPass then
 		self:say("NickServ", "IDENTIFY "..self.config.NSPass)
