32320e03330c2a5ac4b35fac39478d6d724a3f60
Gael-de-Sailly <gael.chretien@akeonet.com>
2015-02-22 12:19:29 +0100

    Removed useless redefinition for glass (already in glasslike_framed_optional)

--- mods/moreblocks/redefinitions.lua
+++ mods/moreblocks/redefinitions.lua
@@ -47,16 +47,6 @@ minetest.register_craft({
 })
 
 -- Redefinitions of some default nodes:
-
--- Make glass and obsidian glass framed, like the More Blocks glasses:
-minetest.override_item("default:glass", {
-	drawtype = "glasslike_framed",
-})
-
-minetest.override_item("default:obsidian_glass", {
-	drawtype = "glasslike_framed",
-})
-
 -- Let there be light. This makes some nodes let light pass through:
 minetest.override_item("default:ladder", {
 	paramtype = "light",
