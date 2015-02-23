2524d461effe9b216e009bda97615daca4e55d38
Ombridride <Ombridride@users.noreply.github.com>
2015-02-23 01:41:40 +0100

    -10 is useless, -1 is better

--- mods/mobs/spider.lua
+++ mods/mobs/spider.lua
@@ -68,7 +68,7 @@ mobs:register_mob("mobs:spider", {
 	step = 1,
 	blood_texture = "mobs_blood.png",
 })
-mobs:register_spawn("mobs:spider", {"default:junglegrass", "default:jungleleaves", "default:jungletree"}, 20, -10, 7000, 1, 31000)
+mobs:register_spawn("mobs:spider", {"default:junglegrass", "default:jungleleaves", "default:jungletree"}, 20, -1, 7000, 1, 31000)
 
 -- Ethereal crystal spike compatibility
 
