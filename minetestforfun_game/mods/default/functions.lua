639584ec06da6d6c6ef6a47ae2f2232cb91f66a9
crabman77 <crabman77.github@gmail.com>
2015-03-03 00:19:08 +0100

    callback for torches in water

--- minetestforfun_game/mods/default/functions.lua
+++ minetestforfun_game/mods/default/functions.lua
@@ -426,3 +426,13 @@ minetest.register_abm({
 })
 
 end -- Ends: if minetest.setting_getbool("leaf_decay") ~= false
+
+minetest.register_on_placenode(function(pos, newnode, placer, oldnode, itemstack, pointed_thing)
+	if newnode.name ~= "default:torch" or minetest.get_item_group(oldnode.name, "water") == 0 then
+		return
+	end
+	minetest.remove_node(pos, newnode)
+	minetest.set_node(pos, oldnode)
+	minetest.add_item(pos, "default:torch")
+end)
+
