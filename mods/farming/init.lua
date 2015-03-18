2f7f6711af14a60c17c8837d6a4af1d5aa899b58
LeMagnesium <mg.minetest@gmail.com>
2015-03-18 19:40:33 +0100

    Reverted removed modification

--- mods/farming/init.lua
+++ mods/farming/init.lua
@@ -90,7 +90,7 @@ minetest.register_abm({
 		if plant == "farming:cocoa_"
 		and minetest.find_node_near(pos, 1, {"default:jungletree", "moretrees:jungletree_leaves_green"}) then
 
-			if minetest.get_node_light(pos) < 13 then return end
+			if minetest.get_node_light(pos) <= 11 then return end
 
 		else
 		
