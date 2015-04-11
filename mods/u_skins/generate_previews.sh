32e911db37b8bacac2922c0306eae370786d298e
crabman77 <crabman77.github@gmail.com>
2015-04-11 22:28:46 +0200

    remove character_*_preview_back.png added .blend files to make preview adapte to add others skin

--- mods/u_skins/generate_previews.sh
+++ mods/u_skins/generate_previews.sh
@@ -8,8 +8,8 @@
 # Warning: any file in .previews/ and u_skins/textures might be deleted without asking.
 PNGCRUSH=true
 IMAGEMAGICK=true
-cd .previews
-rm ../u_skins/textures/*_preview*.png # Remove all previous previews
+cd previews
+rm ../u_skins/textures/character_*_preview*.png # Remove all previous previews
 blender -b skin_previews.blend --python-text "Generate previews" > /dev/null
 if $IMAGEMAGICK
 	then echo "Stripping metadata from generated files..."
@@ -23,26 +23,27 @@ do
 	out_file=output/"$out_name"_preview.png
 	if $IMAGEMAGICK
 	then
-		convert -strip $i $out_file
+		convert -strip -resize 32x64 $i $out_file
 	else
 		mv $i $out_file
 	fi
 done
-for i in blender_out/character_*_01.png;
-do
-	out_name=$(basename $i | sed -e 's/_01.png//g')
-	out_file=output/"$out_name"_preview_back.png
-	if $IMAGEMAGICK
-	then
-		convert -strip $i $out_file
-	else
-		mv $i $out_file
-	fi
-done
+
+#for i in blender_out/character_*_01.png;
+#do
+#	out_name=$(basename $i | sed -e 's/_01.png//g')
+#	out_file=output/"$out_name"_preview_back.png
+#	if $IMAGEMAGICK
+#	then
+#		convert -strip -resize 32x64 $i $out_file
+#	else
+#		mv $i $out_file
+#	fi
+#done
 if $PNGCRUSH
 	then
 		echo "Running pngcrush..."
-		pngcrush -d ../u_skins/textures/ output/*_preview*.png 2> pngcrush_output
-	else mv output/*_preview*.png ../u_skins/textures/
+		pngcrush -d ../u_skins/textures/ output/character_*_preview*.png 2> pngcrush_output
+	else mv output/character_*_preview*.png ../u_skins/textures/
 fi
 echo "Done !"
