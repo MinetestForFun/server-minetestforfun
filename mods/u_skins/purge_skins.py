d042026d0d59883d27bc5ef298043bebf1e65fbf
crabman77 <crabman77.github@gmail.com>
2015-05-02 17:20:40 +0200

    added python script to delete skins removed when updatedb readd     -simply write int number of removed skin in removed_skins.txt (1 by line)

---
+++ mods/u_skins/purge_skins.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import sys, os
+
+# if used on windows ??
+# use os.path.join() -> path sep win "\" or linux "/"
+p_textures = os.path.join("u_skins", "textures")
+p_meta = os.path.join("u_skins", "meta")
+
+try:
+	f = open("removed_skins.txt", "r")
+	skins_exclued = f.readlines()
+except IOError as err:
+	sys.stderr.write("%s\n" % err)
+	sys.exit(1)
+else:
+	f.close()
+
+print("il y a %d skins exclus." % len(skins_exclued))
+
+for skin in skins_exclued:
+	# if not int value, ignore
+	try:
+		skin = "character_%s" % int(skin.strip())
+	except ValueError as err:
+		sys.stderr.write("%s\n" % err)
+		continue
+	# for texture, preview and meta files
+	for f_skin in ( os.path.join(p_textures,"%s.png" % skin), 
+					os.path.join(p_textures,"%s_preview.png" % skin), 
+					os.path.join(p_meta, "%s.txt" % skin) ):
+		if os.path.exists(f_skin):
+			try:
+				os.remove(f_skin)
+			except exception as err:
+				print(err)
+				pass
+			else:
+				print('skin "%s" effacé' % f_skin)
+
+sys.exit(0)
