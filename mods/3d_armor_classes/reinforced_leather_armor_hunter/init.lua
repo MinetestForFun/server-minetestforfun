f84f337c0531de18221c80bd42cbf8959bd3dd22
Ombridride <Ombridride@users.noreply.github.com>
2015-05-22 23:11:23 +0200

    Update reinforced armor

--- mods/3d_armor_classes/reinforced_leather_armor_hunter/init.lua
+++ mods/3d_armor_classes/reinforced_leather_armor_hunter/init.lua
@@ -3,7 +3,7 @@
 minetest.register_tool(":3d_armor:helmet_reinforced_leather_hunter", {
 	description = "Hunter's Reinforced Leather Helmet",
 	inventory_image = ":3d_armor_inv_helmet_reinforced_leather_hunter.png",
-	groups = {armor_head = 10, armor_heal = 0, armor_use = 500}, 		--Valeurs non décidés
+	groups = {armor_head = 10, armor_heal = 0, armor_use = 500}, 		-- Valeurs décidés, a mettre en fonction du spreadsheet
 	wear = 0,
 })
 
@@ -12,7 +12,7 @@ minetest.register_tool(":3d_armor:helmet_reinforced_leather_hunter", {
 minetest.register_tool(":3d_armor:chestplate_reinforced_leather_hunter", {
 	description = "Hunter's Reinforced Leather Chestplate",
 	inventory_image = ":3d_armor_inv_chestplate_reinforced_leather_hunter.png",
-	groups = {armor_torso = 15, armor_heal = 0, armor_use = 500}, 		--Valeurs non décidés
+	groups = {armor_torso = 15, armor_heal = 0, armor_use = 500}, 		-- Valeurs décidés, a mettre en fonction du spreadsheet
 	wear = 0,
 })
 
@@ -21,7 +21,7 @@ minetest.register_tool(":3d_armor:chestplate_reinforced_leather_hunter", {
 minetest.register_tool(":3d_armor:leggings_reinforced_leather_hunter", {
 	description = "Hunter's Reinforced Leather Leggings",
 	inventory_image = ":3d_armor_inv_leggings_reinforced_leather_hunter.png",
-	groups = {armor_legs = 15, armor_heal = 0, armor_use = 500}, 		--Valeurs non décidés
+	groups = {armor_legs = 15, armor_heal = 0, armor_use = 500}, 		-- Valeurs décidés, a mettre en fonction du spreadsheet
 	wear = 0,
 })
 
@@ -30,6 +30,6 @@ minetest.register_tool(":3d_armor:leggings_reinforced_leather_hunter", {
 	minetest.register_tool(":3d_armor:boots_reinforced_leather_hunter", {
 	description = "Hunter's Reinforced Leather Boots",
 	inventory_image = ":3d_armor_inv_boots_reinforced_leather_hunter.png",
-		groups = {armor_feet = 10, armor_heal = 0, armor_use = 500}, 	--Valeurs non décidés
+		groups = {armor_feet = 10, armor_heal = 0, armor_use = 500}, 	-- Valeurs décidés, a mettre en fonction du spreadsheet
 	wear = 0,
 })
