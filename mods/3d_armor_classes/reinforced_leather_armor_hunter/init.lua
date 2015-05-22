0dbcf55dd3e9218eeaa692d86bf4a584b615f873
LeMagnesium <mg.minetest@gmail.com>
2015-05-22 23:43:31 +0200

    Fixed mistakes with automatic replace of textures' name

--- mods/3d_armor_classes/reinforced_leather_armor_hunter/init.lua
+++ mods/3d_armor_classes/reinforced_leather_armor_hunter/init.lua
@@ -2,7 +2,7 @@
 
 minetest.register_tool(":3d_armor:helmet_reinforced_leather_hunter", {
 	description = "Hunter's Reinforced Leather Helmet",
-	inventory_image = ":3d_armor_inv_helmet_reinforced_leather_hunter.png",
+	inventory_image = "3d_armor_inv_helmet_reinforced_leather_hunter.png",
 	groups = {armor_head = 10, armor_heal = 0, armor_use = 500}, 		-- Valeurs décidés, a mettre en fonction du spreadsheet
 	wear = 0,
 })
@@ -11,7 +11,7 @@ minetest.register_tool(":3d_armor:helmet_reinforced_leather_hunter", {
 
 minetest.register_tool(":3d_armor:chestplate_reinforced_leather_hunter", {
 	description = "Hunter's Reinforced Leather Chestplate",
-	inventory_image = ":3d_armor_inv_chestplate_reinforced_leather_hunter.png",
+	inventory_image = "3d_armor_inv_chestplate_reinforced_leather_hunter.png",
 	groups = {armor_torso = 15, armor_heal = 0, armor_use = 500}, 		-- Valeurs décidés, a mettre en fonction du spreadsheet
 	wear = 0,
 })
@@ -20,7 +20,7 @@ minetest.register_tool(":3d_armor:chestplate_reinforced_leather_hunter", {
 
 minetest.register_tool(":3d_armor:leggings_reinforced_leather_hunter", {
 	description = "Hunter's Reinforced Leather Leggings",
-	inventory_image = ":3d_armor_inv_leggings_reinforced_leather_hunter.png",
+	inventory_image = "3d_armor_inv_leggings_reinforced_leather_hunter.png",
 	groups = {armor_legs = 15, armor_heal = 0, armor_use = 500}, 		-- Valeurs décidés, a mettre en fonction du spreadsheet
 	wear = 0,
 })
@@ -29,7 +29,7 @@ minetest.register_tool(":3d_armor:leggings_reinforced_leather_hunter", {
 
 	minetest.register_tool(":3d_armor:boots_reinforced_leather_hunter", {
 	description = "Hunter's Reinforced Leather Boots",
-	inventory_image = ":3d_armor_inv_boots_reinforced_leather_hunter.png",
+	inventory_image = "3d_armor_inv_boots_reinforced_leather_hunter.png",
 		groups = {armor_feet = 10, armor_heal = 0, armor_use = 500}, 	-- Valeurs décidés, a mettre en fonction du spreadsheet
 	wear = 0,
 })
