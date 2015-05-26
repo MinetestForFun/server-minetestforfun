c2d45414690b8247bcf3e1375b5dcdb1c058e200
Ombridride <Ombridride@users.noreply.github.com>
2015-05-26 19:10:37 +0200

    Remove useless comments

--- mods/3d_armor_classes/leather_armor_hunter/init.lua
+++ mods/3d_armor_classes/leather_armor_hunter/init.lua
@@ -3,7 +3,7 @@
 minetest.register_tool(":3d_armor:helmet_leather_hunter", {
 	description = "Hunter's Leather Helmet",
 	inventory_image = "3d_armor_inv_helmet_leather_hunter.png",
-	groups = {armor_head = 5, armor_heal = 0, armor_use = 250}, 		-- Valeurs décidés, a mettre en fonction du spreadsheet
+	groups = {armor_head = 5, armor_heal = 0, armor_use = 250},
 	wear = 0,
 })
 
@@ -12,7 +12,7 @@ minetest.register_tool(":3d_armor:helmet_leather_hunter", {
 minetest.register_tool(":3d_armor:chestplate_leather_hunter", {
 	description = "Hunter's Leather Chestplate",
 	inventory_image = "3d_armor_inv_chestplate_leather_hunter.png",
-	groups = {armor_torso = 9, armor_heal = 0, armor_use = 250}, 		-- Valeurs décidés, a mettre en fonction du spreadsheet
+	groups = {armor_torso = 9, armor_heal = 0, armor_use = 250},
 	wear = 0,
 })
 
@@ -21,7 +21,7 @@ minetest.register_tool(":3d_armor:chestplate_leather_hunter", {
 minetest.register_tool(":3d_armor:leggings_leather_hunter", {
 	description = "Hunter's Leather Leggings",
 	inventory_image = "3d_armor_inv_leggings_leather_hunter.png",
-	groups = {armor_legs = 9, armor_heal = 0, armor_use = 250}, 		-- Valeurs décidés, a mettre en fonction du spreadsheet
+	groups = {armor_legs = 9, armor_heal = 0, armor_use = 250},
 	wear = 0,
 })
 
@@ -30,6 +30,6 @@ minetest.register_tool(":3d_armor:leggings_leather_hunter", {
 	minetest.register_tool(":3d_armor:boots_leather_hunter", {
 	description = "Hunter's Leather Boots",
 	inventory_image = "3d_armor_inv_boots_leather_hunter.png",
-		groups = {armor_feet = 5, armor_heal = 0, armor_use = 250}, 	-- Valeurs décidés, a mettre en fonction du spreadsheet
+		groups = {armor_feet = 5, armor_heal = 0, armor_use = 250},
 	wear = 0,
 })
