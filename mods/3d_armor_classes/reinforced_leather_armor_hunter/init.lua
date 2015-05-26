2367a1b8538db6b6140d0e6fce60241bf96c2d06
Ombridride <Ombridride@users.noreply.github.com>
2015-05-26 23:31:31 +0200

    Finish the "reinforced_leather" armor

--- mods/3d_armor_classes/reinforced_leather_armor_hunter/init.lua
+++ mods/3d_armor_classes/reinforced_leather_armor_hunter/init.lua
@@ -1,38 +1,33 @@
--- Register helmets:
-
-minetest.register_tool(":3d_armor:helmet_reinforced_leather_hunter", {
-	description = "Hunter's Reinforced Leather Helmet",
-	inventory_image = "3d_armor_inv_helmet_reinforced_leather_hunter.png",
-	groups = {armor_head = 6, armor_heal = 0, armor_use = 40},
-	wear = 0,
-})
-
--- Register chestplates:
-
-minetest.register_tool(":3d_armor:chestplate_reinforced_leather_hunter", {
-	description = "Hunter's Reinforced Leather Chestplate",
-	inventory_image = "3d_armor_inv_chestplate_reinforced_leather_hunter.png",
-	groups = {armor_torso = 10, armor_heal = 0, armor_use = 40},
-	wear = 0,
-})
-
--- Register leggings:
-
-minetest.register_tool(":3d_armor:leggings_reinforced_leather_hunter", {
-	description = "Hunter's Reinforced Leather Leggings",
-	inventory_image = "3d_armor_inv_leggings_reinforced_leather_hunter.png",
-	groups = {armor_legs = 10, armor_heal = 0, armor_use = 40},
-	wear = 0,
-})
-
--- Register boots:
-
-	minetest.register_tool(":3d_armor:boots_reinforced_leather_hunter", {
-	description = "Hunter's Reinforced Leather Boots",
-	inventory_image = "3d_armor_inv_boots_reinforced_leather_hunter.png",
-	groups = {armor_feet = 6, armor_heal = 0, armor_use = 40},
-	wear = 0,
-})
+if ARMOR_MATERIALS.reinforced_leather then
+	-- Register helmets :
+	minetest.register_tool(":3d_armor:helmet_reinforced_leather_hunter", {
+		description = "Hunter's Reinforced Leather Helmet",
+		inventory_image = "3d_armor_inv_helmet_reinforced_leather_hunter.png",
+		groups = {armor_head = 6, armor_heal = 0, armor_use = 40},
+		wear = 0,
+	})
+	-- Register chestplates :
+	minetest.register_tool(":3d_armor:chestplate_reinforced_leather_hunter", {
+		description = "Hunter's Reinforced Leather Chestplate",
+		inventory_image = "3d_armor_inv_chestplate_reinforced_leather_hunter.png",
+		groups = {armor_torso = 10, armor_heal = 0, armor_use = 40},
+		wear = 0,
+	})
+	-- Register leggings :
+	minetest.register_tool(":3d_armor:leggings_reinforced_leather_hunter", {
+		description = "Hunter's Reinforced Leather Leggings",
+		inventory_image = "3d_armor_inv_leggings_reinforced_leather_hunter.png",
+		groups = {armor_legs = 10, armor_heal = 0, armor_use = 40},
+		wear = 0,
+	})
+	-- Register boots :
+		minetest.register_tool(":3d_armor:boots_reinforced_leather_hunter", {
+		description = "Hunter's Reinforced Leather Boots",
+		inventory_image = "3d_armor_inv_boots_reinforced_leather_hunter.png",
+		groups = {armor_feet = 6, armor_heal = 0, armor_use = 40},
+		wear = 0,
+	})
+end
 
 -- Reinforced Leather craft recipe
 minetest.register_craft({
