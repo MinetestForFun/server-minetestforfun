4c4abb3830af0c1ce47781226be003a4e420d9d3
Ombridride <Ombridride@users.noreply.github.com>
2015-05-29 01:07:17 +0200

    Set "armor_heal" to 0 for default armors

--- mods/3d_armor/3d_armor/init.lua
+++ mods/3d_armor/3d_armor/init.lua
@@ -87,25 +87,25 @@ if ARMOR_MATERIALS.bronze then
 	minetest.register_tool("3d_armor:helmet_bronze", {
 		description = "Bronze Helmet",
 		inventory_image = "3d_armor_inv_helmet_bronze.png",
-		groups = {armor_head=2.5, armor_heal=4, armor_use=250},
+		groups = {armor_head=2.5, armor_heal=0, armor_use=250},
 		wear = 0,
 	})
 	minetest.register_tool("3d_armor:chestplate_bronze", {
 		description = "Bronze Chestplate",
 		inventory_image = "3d_armor_inv_chestplate_bronze.png",
-		groups = {armor_torso=5.5, armor_heal=4, armor_use=250},
+		groups = {armor_torso=5.5, armor_heal=0, armor_use=250},
 		wear = 0,
 	})
 	minetest.register_tool("3d_armor:leggings_bronze", {
 		description = "Bronze Leggings",
 		inventory_image = "3d_armor_inv_leggings_bronze.png",
-		groups = {armor_legs=5.5, armor_heal=6, armor_use=250},
+		groups = {armor_legs=5.5, armor_heal=0, armor_use=250},
 		wear = 0,
 	})
 	minetest.register_tool("3d_armor:boots_bronze", {
 		description = "Bronze Boots",
 		inventory_image = "3d_armor_inv_boots_bronze.png",
-		groups = {armor_feet=2.5, armor_heal=4, armor_use=250},
+		groups = {armor_feet=2.5, armor_heal=0, armor_use=250},
 		wear = 0,
 	})
 end
@@ -114,25 +114,25 @@ if ARMOR_MATERIALS.diamond then
 	minetest.register_tool("3d_armor:helmet_diamond", {
 		description = "Diamond Helmet",
 		inventory_image = "3d_armor_inv_helmet_diamond.png",
-		groups = {armor_head=4, armor_heal=12, armor_use=100},
+		groups = {armor_head=4, armor_heal=0, armor_use=100},
 		wear = 0,
 	})
 	minetest.register_tool("3d_armor:chestplate_diamond", {
 		description = "Diamond Chestplate",
 		inventory_image = "3d_armor_inv_chestplate_diamond.png",
-		groups = {armor_torso=8, armor_heal=12, armor_use=100},
+		groups = {armor_torso=8, armor_heal=0, armor_use=100},
 		wear = 0,
 	})
 	minetest.register_tool("3d_armor:leggings_diamond", {
 		description = "Diamond Leggings",
 		inventory_image = "3d_armor_inv_leggings_diamond.png",
-		groups = {armor_legs=8, armor_heal=12, armor_use=100},
+		groups = {armor_legs=8, armor_heal=0, armor_use=100},
 		wear = 0,
 	})
 	minetest.register_tool("3d_armor:boots_diamond", {
 		description = "Diamond Boots",
 		inventory_image = "3d_armor_inv_boots_diamond.png",
-		groups = {armor_feet=4, armor_heal=12, armor_use=100},
+		groups = {armor_feet=4, armor_heal=0, armor_use=100},
 		wear = 0,
 	})
 end
@@ -141,25 +141,25 @@ if ARMOR_MATERIALS.gold then
 	minetest.register_tool("3d_armor:helmet_gold", {
 		description = "Gold Helmet",
 		inventory_image = "3d_armor_inv_helmet_gold.png",
-		groups = {armor_head=3, armor_heal=6, armor_use=200},
+		groups = {armor_head=3, armor_heal=0, armor_use=200},
 		wear = 0,
 	})
 	minetest.register_tool("3d_armor:chestplate_gold", {
 		description = "Gold Chestplate",
 		inventory_image = "3d_armor_inv_chestplate_gold.png",
-		groups = {armor_torso=7, armor_heal=6, armor_use=200},
+		groups = {armor_torso=7, armor_heal=0, armor_use=200},
 		wear = 0,
 	})
 	minetest.register_tool("3d_armor:leggings_gold", {
 		description = "Gold Leggings",
 		inventory_image = "3d_armor_inv_leggings_gold.png",
-		groups = {armor_legs=7, armor_heal=6, armor_use=200},
+		groups = {armor_legs=7, armor_heal=0, armor_use=200},
 		wear = 0,
 	})
 	minetest.register_tool("3d_armor:boots_gold", {
 		description = "Gold Boots",
 		inventory_image = "3d_armor_inv_boots_gold.png",
-		groups = {armor_feet=3, armor_heal=6, armor_use=200},
+		groups = {armor_feet=3, armor_heal=0, armor_use=200},
 		wear = 0,
 	})
 end
@@ -168,25 +168,25 @@ if ARMOR_MATERIALS.mithril then
 	minetest.register_tool("3d_armor:helmet_mithril", {
 		description = "Mithril Helmet",
 		inventory_image = "3d_armor_inv_helmet_mithril.png",
-		groups = {armor_head=5, armor_heal=12, armor_use=50},
+		groups = {armor_head=5, armor_heal=0, armor_use=50},
 		wear = 0,
 	})
 	minetest.register_tool("3d_armor:chestplate_mithril", {
 		description = "Mithril Chestplate",
 		inventory_image = "3d_armor_inv_chestplate_mithril.png",
-		groups = {armor_torso=11, armor_heal=12, armor_use=50},
+		groups = {armor_torso=11, armor_heal=0, armor_use=50},
 		wear = 0,
 	})
 	minetest.register_tool("3d_armor:leggings_mithril", {
 		description = "Mithril Leggings",
 		inventory_image = "3d_armor_inv_leggings_mithril.png",
-		groups = {armor_legs=11, armor_heal=12, armor_use=50},
+		groups = {armor_legs=11, armor_heal=0, armor_use=50},
 		wear = 0,
 	})
 	minetest.register_tool("3d_armor:boots_mithril", {
 		description = "Mithril Boots",
 		inventory_image = "3d_armor_inv_boots_mithril.png",
-		groups = {armor_feet=5, armor_heal=12, armor_use=50},
+		groups = {armor_feet=5, armor_heal=0, armor_use=50},
 		wear = 0,
 	})
 end
@@ -195,25 +195,25 @@ if ARMOR_MATERIALS.crystal then
 	minetest.register_tool("3d_armor:helmet_crystal", {
 		description = "Crystal Helmet",
 		inventory_image = "3d_armor_inv_helmet_crystal.png",
-		groups = {armor_head=15, armor_heal=12, armor_use=50, armor_fire=1},
+		groups = {armor_head=15, armor_heal=0, armor_use=50, armor_fire=1},
 		wear = 0,
 	})
 	minetest.register_tool("3d_armor:chestplate_crystal", {
 		description = "Crystal Chestplate",
 		inventory_image = "3d_armor_inv_chestplate_crystal.png",
-		groups = {armor_torso=20, armor_heal=12, armor_use=50, armor_fire=1},
+		groups = {armor_torso=20, armor_heal=0, armor_use=50, armor_fire=1},
 		wear = 0,
 	})
 	minetest.register_tool("3d_armor:leggings_crystal", {
 		description = "Crystal Leggings",
 		inventory_image = "3d_armor_inv_leggings_crystal.png",
-		groups = {armor_legs=20, armor_heal=12, armor_use=50, armor_fire=1},
+		groups = {armor_legs=20, armor_heal=0, armor_use=50, armor_fire=1},
 		wear = 0,
 	})
 	minetest.register_tool("3d_armor:boots_crystal", {
 		description = "Crystal Boots",
 		inventory_image = "3d_armor_inv_boots_crystal.png",
-		groups = {armor_feet=15, armor_heal=12, armor_use=50, physics_speed=1, physics_jump=0.5, armor_fire=1},
+		groups = {armor_feet=15, armor_heal=0, armor_use=50, physics_speed=1, physics_jump=0.5, armor_fire=1},
 		wear = 0,
 	})
 end
