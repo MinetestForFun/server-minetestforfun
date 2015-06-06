c815abe5952ce1a8c271743417102d14904f9418
LeMagnesium <mg.minetest@gmail.com>
2015-06-06 15:38:01 +0200

    Updated flowers' mod after merge in Minetest_game

--- minetestforfun_game/mods/flowers/init.lua
+++ minetestforfun_game/mods/flowers/init.lua
@@ -1,6 +1,9 @@
 -- Minetest 0.4 mod: default
 -- See README.txt for licensing and other information.
 
+-- Namespace for functions
+flowers = {}
+
 -- Map Generation
 dofile(minetest.get_modpath("flowers").."/mapgen.lua")
 
@@ -12,71 +15,47 @@ minetest.register_alias("flowers:flower_rose", "flowers:rose")
 minetest.register_alias("flowers:flower_tulip", "flowers:tulip")
 minetest.register_alias("flowers:flower_viola", "flowers:viola")
 
----------------------------------
---- Simple flowers (one node) ---
----------------------------------
+-- Flower registration function
+local function add_simple_flower(name, desc, box, f_groups)
+	-- Common flowers' groups
+	f_groups.snappy = 3
+	f_groups.flammable = 2
+	f_groups.flower = 1
+	f_groups.flora = 1
+	f_groups.attached_node = 1
 
-local function add_simple_flower(name, desc, image, box, f_groups)
 	minetest.register_node("flowers:"..name.."", {
 		description = desc,
 		drawtype = "plantlike",
-		tiles = { image..".png" },
-		inventory_image = image..".png",
-		wield_image = image..".png",
+		tiles = { "flowers_" .. name .. ".png" },
+		inventory_image = "flowers_" .. name .. ".png",
+		wield_image = "flowers_" .. name .. ".png",
 		sunlight_propagates = true,
 		paramtype = "light",
 		walkable = false,
 		stack_max = 99,
 		groups = f_groups,
 		sounds = default.node_sound_leaves_defaults(),
-		selection_box = box
+		selection_box = {
+			type = "fixed",
+			fixed = box
+		}
 	})
 end
 
--- Selection boxes
-vertical = {
-	type = "fixed",
-	fixed = { -0.15, -0.5, -0.15, 0.15, 0.2, 0.15 },
+-- Registrations using the function above
+flowers.datas = {
+	{"dandelion_yellow", "Yellow Dandelion", { -0.15, -0.5, -0.15, 0.15, 0.2, 0.15 }, {color_yellow=1}},
+	{"geranium", "Blue Geranium", { -0.15, -0.5, -0.15, 0.15, 0.2, 0.15 }, {color_blue=1}},
+	{"rose", "Rose", { -0.15, -0.5, -0.15, 0.15, 0.3, 0.15 }, {color_red=1}},
+	{"tulip", "Orange Tulip", { -0.15, -0.5, -0.15, 0.15, 0.2, 0.15 }, {color_orange=1}},
+	{"dandelion_white", "White dandelion", { -0.5, -0.5, -0.5, 0.5, -0.2, 0.5 }, {color_white=1}},
+	{"viola", "Viola", { -0.5, -0.5, -0.5, 0.5, -0.2, 0.5 }, {color_violet=1}}
 }
 
-slab = {
-	type = "fixed",
-	fixed = { -0.5, -0.5, -0.5, 0.5, -0.2, 0.5 },
-}
-
-add_simple_flower("dandelion_yellow", "Yellow Dandelion", "flowers_dandelion_yellow", vertical, {snappy=3,flammable=2,flower=1,flora=1,attached_node=1,dig_by_water=1,color_yellow=1})
-add_simple_flower("geranium", "Blue Geranium", "flowers_geranium", vertical, {snappy=3,flammable=2,flower=1,flora=1,attached_node=1,dig_by_water=1,color_blue=1})
-add_simple_flower("rose", "Rose", "flowers_rose", vertical, 
-{snappy=3,flammable=2,flower=1,flora=1,attached_node=1,dig_by_water=1,color_red=1}) 
-add_simple_flower("tulip", "Orange Tulip", "flowers_tulip", vertical,
-{snappy=3,flammable=2,flower=1,flora=1,attached_node=1,dig_by_water=1,color_orange=1})
-add_simple_flower("dandelion_white", "White dandelion", "flowers_dandelion_white", slab, {snappy=3,flammable=2,flower=1,flora=1,attached_node=1,color_white=1})
-add_simple_flower("viola", "Viola", "flowers_viola", slab, 
-{snappy=3,flammable=2,flower=1,flora=1,attached_node=1,color_violet=1})
-
-minetest.register_node("flowers:lily_pad", {
-	description = "Lily Pad",
-	drawtype = "nodebox",
-	tiles = { "flowers_lily_pad.png" },
-	inventory_image = "flowers_lily_pad.png",
-	wield_image = "flowers_lily_pad.png",
-	wield_scale = {x = 1, y = 1, z = 0.001},
-	is_ground_content = true,
-	sunlight_propagates = true,
-	paramtype = "light",
-	walkable = false,
-	buildable_to = true,
-	groups = {snappy = 3, flammable = 2, flower = 1, flora = 1},
-	sounds = default.node_sound_leaves_defaults(),
-	node_box = {
-		type = "fixed",
-		fixed = {-0.5, -0.45, -0.5, 0.5, -0.4375, 0.5},
-	},
-	selection_box = {
-		type = "fixed",
-		fixed = {-0.5, -0.5, -0.5, 0.5, -0.4375, 0.5},
-	},
-})
+for _,item in pairs(flowers.datas) do
+	add_simple_flower(unpack(item))
+end
 
 minetest.register_abm({
 	nodenames = {"group:flora"},
@@ -89,17 +68,25 @@ minetest.register_abm({
 		pos.y = pos.y + 1
 		if under.name == "default:desert_sand" then
 			minetest.set_node(pos, {name="default:dry_shrub"})
-		elseif under.name ~= "default:dirt_with_grass" then return end
+		elseif under.name ~= "default:dirt_with_grass" then
+			return
+		end
 		
 		local light = minetest.get_node_light(pos)
-		if not light or light < 13 then return end
+		if not light or light < 13 then
+			return
+		end
 		
-		local pos0 = {x = pos.x - 4, y = pos.y - 4, z = pos.z - 4}
-		local pos1 = {x = pos.x + 4, y = pos.y + 4, z = pos.z + 4}
-		if #minetest.find_nodes_in_area(pos0, pos1, "group:flora_block") > 0 then return end
+		local pos0 = {x=pos.x-4,y=pos.y-4,z=pos.z-4}
+		local pos1 = {x=pos.x+4,y=pos.y+4,z=pos.z+4}
+		if #minetest.find_nodes_in_area(pos0, pos1, "group:flora_block") > 0 then
+			return
+		end
 		
 		local flowers = minetest.find_nodes_in_area(pos0, pos1, "group:flora")
-		if #flowers > 3 then return end
+		if #flowers > 3 then
+			return
+		end
 		
 		local seedling = minetest.find_nodes_in_area(pos0, pos1, "default:dirt_with_grass")
 		if #seedling > 0 then
@@ -115,7 +102,3 @@ minetest.register_abm({
 		end
 	end,
 })
-
-if minetest.setting_getbool("log_mods") then
-	minetest.log("action", "Carbone: [flowers] loaded.")
-end
