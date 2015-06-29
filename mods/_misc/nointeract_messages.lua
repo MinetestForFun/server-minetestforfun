11a121f7dff89ff43beaf14e193743af9f9e5890
LeMagnesium <mg.minetest@gmail.com>
2015-06-29 20:19:40 +0200

    Fixed /rules and added blacklist system  - Removed useless copy of /rules  - Added a blacklist system avoiding people to get back the interact privilege using /rules when blacklisted

--- mods/_misc/nointeract_messages.lua
+++ mods/_misc/nointeract_messages.lua
@@ -6,9 +6,11 @@ minetest.register_globalstep(function(dtime)
 	for _,player in ipairs(minetest.get_connected_players()) do
 		local name = player:get_player_name()
 		if not minetest.check_player_privs(name, {interact=true}) then
-			if minetest.check_player_privs(name, {shout=true}) then
-				minetest.chat_send_player(name, "Hey " .. name .. " ! Pour pouvoir construire et intéragir sur ce serveur, tu dois lire les règles du serveur et les accepter. Tape /rules.")
-				minetest.chat_send_player(name, "Hey " .. name .. " ! To build and interact on this server, you have to read the rules of our server and agree them. Type /rules.")
+			if not interact.blacklist[name] then
+				if minetest.check_player_privs(name, {shout=true}) then
+					minetest.chat_send_player(name, "Hey " .. name .. " ! Pour pouvoir construire et intéragir sur ce serveur, tu dois lire les règles du serveur et les accepter. Tape /rules.")
+					minetest.chat_send_player(name, "Hey " .. name .. " ! To build and interact on this server, you have to read the rules of our server and agree them. Type /rules.")
+				end
 			end
 		end
 	end
