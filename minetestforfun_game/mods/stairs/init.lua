MinetestForFun/server-minetestforfun

commit 169327abceb04cafa8449bd69d14bad69a4021b4

Update [stairs]

minetestforfun_game/mods/stairs/init.lua
@@ -7,14 +7,22 @@ stairs = {}
 function stairs.register_stair(subname, recipeitem, groups, images, description, sounds)
 	minetest.register_node(":stairs:stair_" .. subname, {
 		description = description,
-		drawtype = "nodebox",
+		drawtype = "mesh",
+		mesh = "stairs.obj",
 		tiles = images,
 		paramtype = "light",
 		paramtype2 = "facedir",
 		is_ground_content = false,
 		groups = groups,
 		sounds = sounds,
-		node_box = {
+		selection_box = {
+			type = "fixed",
+			fixed = {
+				{-0.5, -0.5, -0.5, 0.5, 0, 0.5},
+				{-0.5, 0, 0, 0.5, 0.5, 0.5},
+			},
+		},
+		collision_box = {
 			type = "fixed",
 			fixed = {
 				{-0.5, -0.5, -0.5, 0.5, 0, 0.5},
@@ -138,7 +146,7 @@ function stairs.register_slab(subname, recipeitem, groups, images, description, 
 				end
 				return itemstack
 			end
-
+			
 			-- Upside down slabs
 			if p0.y-1 == p1.y then
 				-- Turn into full block if pointing at a existing slab
@@ -267,7 +275,7 @@ stairs.register_stair_and_slab("sandstone", "default:sandstone",
 		"Sandstone Stair",
 		"Sandstone Slab",
 		default.node_sound_stone_defaults())
-
+		
 stairs.register_stair_and_slab("sandstonebrick", "default:sandstonebrick",
 		{crumbly=2,cracky=2},
 		{"default_sandstone_brick.png"},
@@ -309,13 +317,3 @@ stairs.register_stair_and_slab("obsidianbrick", "default:obsidianbrick",
 		"Obsidian Brick Stair",
 		"Obsidian Brick Slab",
 		default.node_sound_stone_defaults())
-
--- From BFD:
-
-stairs.register_stair_and_slab("cherry_wood", "default:cherry_plank",
-		{snappy=2,choppy=2,oddly_breakable_by_hand=2,flammable=3},
-		{"default_wood_cherry_planks.png"},
-		"Cherry Plank Stair",
-		"Cherry Plank Slab",
-		"Cherry Plank Corner Stair",
-		default.node_sound_wood_defaults())

minetestforfun_game/mods/stairs/models/stairs.obj
@@ -1,0 +1,39 @@
+# Blender v2.72 (sub 0) OBJ File: ''
+# www.blender.org
+o stair
+v -0.500000 -0.500000 0.500000
+v -0.500000 -0.000000 0.500000
+v -0.500000 0.000000 -0.500000
+v -0.500000 -0.500000 -0.500000
+v 0.500000 -0.500000 0.500000
+v 0.500000 -0.500000 -0.500000
+v 0.500000 0.000000 -0.500000
+v 0.500000 -0.000000 0.500000
+v -0.500000 -0.000000 0.500000
+v -0.500000 0.500000 0.500000
+v -0.500000 0.500000 0.000000
+v -0.500000 0.000000 0.000000
+v 0.500000 -0.000000 0.500000
+v 0.500000 0.000000 0.000000
+v 0.500000 0.500000 0.000000
+v 0.500000 0.500000 0.500000
+vt 1.000000 0.000000
+vt 1.000000 0.500000
+vt 0.000000 0.500000
+vt 0.000000 0.000000
+vt 1.000000 1.000000
+vt 0.000000 1.000000
+vt 0.500000 1.000000
+vt 0.500000 0.500000
+s off
+f 1/1 2/2 3/3 4/4
+f 5/4 6/1 7/2 8/3
+f 1/4 5/1 8/2 2/3
+f 4/1 3/2 7/3 6/4
+f 1/1 4/5 6/6 5/4
+f 2/5 8/6 7/4 3/1
+f 9/2 10/5 11/7 12/8
+f 13/3 14/8 15/7 16/6
+f 9/3 13/2 16/5 10/6
+f 12/2 11/5 15/6 14/3
+f 10/5 16/6 15/3 11/2 

commit 40e93d49229cfd39a5b58c2c6272aa53434514e2

Fix error introduced in 169327abceb04cafa8449bd69d14bad69a4021b4

minetestforfun_game/mods/stairs/init.lua
@@ -317,3 +317,13 @@ stairs.register_stair_and_slab("obsidianbrick", "default:obsidianbrick",
 		"Obsidian Brick Stair",
 		"Obsidian Brick Slab",
 		default.node_sound_stone_defaults())
+
+-- From BFD:
+
+stairs.register_stair_and_slab("cherry_wood", "default:cherry_plank",
+		{snappy=2,choppy=2,oddly_breakable_by_hand=2,flammable=3},
+		{"default_wood_cherry_planks.png"},
+		"Cherry Plank Stair",
+		"Cherry Plank Slab",
+		"Cherry Plank Corner Stair",
+		default.node_sound_wood_defaults())