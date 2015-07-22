0e9ba218d2640e319888137ddde9aa6f74777838
Ombridride <quentinbd@gmail.com>
2015-07-22 02:34:31 +0200

    mithril string, minotaur bow improved, tweak caracteristics, recipes, etc... for the hunter

--- mods/throwing/tools.lua
+++ mods/throwing/tools.lua
@@ -31,7 +31,7 @@ if not DISABLE_STEEL_BOW then
 end
 
 if not DISABLE_ROYAL_BOW then
-	throwing_register_bow ('bow_royal', 'Royal bow', {x=1, y=1.5, z=0.5}, 18, 1.4, 750, false, {
+	throwing_register_bow ('bow_royal', 'Royal bow', {x=1, y=1.5, z=0.5}, 25, 1.4, 750, false, {
 			{'farming:string', 'group:wood', 'default:diamond'},
 			{'farming:string', '', 'default:gold_ingot'},
 			{'farming:string', 'group:wood', 'default:diamond'},
@@ -40,21 +40,19 @@ end
 
 --function throwing_register_bow (name, desc, scale, stiffness, reload_time, toughness, is_cross, craft)
 if not DISABLE_MINOTAUR_HORN_BOW then
-	throwing_register_bow ('bow_minotaur_horn', 'Minotaur Horn Bow', {x=1, y=1.5, z=0.5}, 30, 1, 1000, false, {
+	throwing_register_bow ('bow_minotaur_horn', 'Minotaur Horn Bow', {x=1, y=1.5, z=0.5}, 35, 1.5, 1000, false, {
 			{'farming:string', 'mobs:minotaur_horn', 'mobs:minotaur_horn'},
 			{'farming:string', '', 'moreores:mithril_ingot'},
 			{'farming:string', 'mobs:minotaur_horn', 'mobs:minotaur_horn'},
 		})
 end
 
--- NOT YET CHOOSEN, material ? craft ? name ?
---[[if not DISABLE_xxx_BOW then
-	throwing_register_bow ('bow_xxx', 'xxx Bow', {x=1, y=1.5, z=0.5}, 35, 0.8, 1250, false, {
-			{'farming:string', 'group:wood', 'default:diamond'},
-			{'farming:string', '', 'default:gold_ingot'},
-			{'farming:string', 'group:wood', 'default:diamond'},
+--function throwing_register_bow (name, desc, scale, stiffness, reload_time, toughness, is_cross, craft)
+if not DISABLE_MINOTAUR_HORN_IMPROVED_BOW then
+	throwing_register_bow ('bow_minotaur_horn_improved', 'Minotaur Horn Improved Bow', {x=1, y=1.5, z=0.5}, 50, 1.5, 2000, false, {
+			{'throwing:bow_minotaur_horn', 'throwing:string_mithril'},
 		})
-end--]]
+end
 
 if not DISABLE_CROSSBOW then
 	throwing_register_bow ('crossbow', 'Crossbow', {x=1, y=1.3, z=0.5}, 28, 5, 80, true, {
@@ -71,3 +69,10 @@ if not DISABLE_ARBALEST then
 			{'default:steel_ingot', 'farming:string', 'default:stick'},
 		})
 end
+
+-- Craft recipe of the "Mithril String"
+minetest.register_craft({
+output = "throwing:string_mithril",
+type = "shapeless",
+recipe = {"moreores:mithril_ingot", "farming:string"},
+})
