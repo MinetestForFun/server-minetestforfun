a1d04f1374d9b8361b9b5dbbbce73764b3370f09
Ombridride <Ombridride@users.noreply.github.com>
2015-07-27 20:37:36 +0200

    Removed the health values on the blackmithril
    armor, From the [3d_armor_classes] modpack

--- mods/3d_armor_classes/blackmithril/init.lua
+++ mods/3d_armor_classes/blackmithril/init.lua
@@ -3,28 +3,28 @@ if ARMOR_MATERIALS.blackmithril then
 	minetest.register_tool(":3d_armor:helmet_blackmithril", {
 		description = "Black Mithril Helmet",
 		inventory_image = "3d_armor_inv_helmet_blackmithril.png",
-		groups = {armor_head = 11, armor_heal = 1, armor_use = 40},
+		groups = {armor_head = 11, armor_heal = 0, armor_use = 40},
 		wear = 0,
 	})
 	-- Register chestplates :
 	minetest.register_tool(":3d_armor:chestplate_blackmithril", {
 		description = "Black Mithril Chestplate",
 		inventory_image = "3d_armor_inv_chestplate_blackmithril.png",
-		groups = {armor_torso = 17, armor_heal = 1, armor_use = 40},
+		groups = {armor_torso = 17, armor_heal = 0, armor_use = 40},
 		wear = 0,
 	})
 	-- Register leggings :
 	minetest.register_tool(":3d_armor:leggings_blackmithril", {
 		description = "Black Mithril Leggings",
 		inventory_image = "3d_armor_inv_leggings_blackmithril.png",
-		groups = {armor_legs = 17, armor_heal = 1, armor_use = 40},
+		groups = {armor_legs = 17, armor_heal = 0, armor_use = 40},
 		wear = 0,
 	})
 	-- Register boots :
 		minetest.register_tool(":3d_armor:boots_blackmithril", {
 		description = "Black Mithril Boots",
 		inventory_image = "3d_armor_inv_boots_blackmithril.png",
-		groups = {armor_feet = 11, armor_heal = 1, armor_use = 40},
+		groups = {armor_feet = 11, armor_heal = 0, armor_use = 40},
 		wear = 0,
 	})
 end
