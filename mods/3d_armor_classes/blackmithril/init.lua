9dd60923064348c04d99d1583709b46f4c48eea6
Ombridride <contact@qbuissondebon.info>
2015-07-29 19:18:10 +0200

    Add the class name in the description of the armor items

--- mods/3d_armor_classes/blackmithril/init.lua
+++ mods/3d_armor_classes/blackmithril/init.lua
@@ -1,28 +1,28 @@
 if ARMOR_MATERIALS.blackmithril then
 	-- Register helmets :
 	minetest.register_tool(":3d_armor:helmet_blackmithril", {
-		description = "Black Mithril Helmet",
+		description = "Black Mithril Helmet (Warrior)",
 		inventory_image = "3d_armor_inv_helmet_blackmithril.png",
 		groups = {armor_head = 11, armor_heal = 0, armor_use = 40},
 		wear = 0,
 	})
 	-- Register chestplates :
 	minetest.register_tool(":3d_armor:chestplate_blackmithril", {
-		description = "Black Mithril Chestplate",
+		description = "Black Mithril Chestplate (Warrior)",
 		inventory_image = "3d_armor_inv_chestplate_blackmithril.png",
 		groups = {armor_torso = 17, armor_heal = 0, armor_use = 40},
 		wear = 0,
 	})
 	-- Register leggings :
 	minetest.register_tool(":3d_armor:leggings_blackmithril", {
-		description = "Black Mithril Leggings",
+		description = "Black Mithril Leggings (Warrior)",
 		inventory_image = "3d_armor_inv_leggings_blackmithril.png",
 		groups = {armor_legs = 17, armor_heal = 0, armor_use = 40},
 		wear = 0,
 	})
 	-- Register boots :
 		minetest.register_tool(":3d_armor:boots_blackmithril", {
-		description = "Black Mithril Boots",
+		description = "Black Mithril Boots (Warrior)",
 		inventory_image = "3d_armor_inv_boots_blackmithril.png",
 		groups = {armor_feet = 11, armor_heal = 0, armor_use = 40},
 		wear = 0,
--- mods/3d_armor_classes/reinforcedleather/init.lua
+++ mods/3d_armor_classes/reinforcedleather/init.lua
@@ -1,28 +1,28 @@
 if ARMOR_MATERIALS.reinforcedleather then
 	-- Register helmets :
 	minetest.register_tool(":3d_armor:helmet_reinforcedleather", {
-		description = "Hunter's Reinforced Leather Helmet",
+		description = "Hunter's Reinforced Leather Helmet (Hunter)",
 		inventory_image = "3d_armor_inv_helmet_reinforcedleather.png",
 		groups = {armor_head = 6, armor_heal = 0, armor_use = 40},
 		wear = 0,
 	})
 	-- Register chestplates :
 	minetest.register_tool(":3d_armor:chestplate_reinforcedleather", {
-		description = "Reinforced Leather Chestplate",
+		description = "Reinforced Leather Chestplate (Hunter)",
 		inventory_image = "3d_armor_inv_chestplate_reinforcedleather.png",
 		groups = {armor_torso = 11, armor_heal = 0, armor_use = 40},
 		wear = 0,
 	})
 	-- Register leggings :
 	minetest.register_tool(":3d_armor:leggings_reinforcedleather", {
-		description = "Reinforced Leather Leggings",
+		description = "Reinforced Leather Leggings (Hunter)",
 		inventory_image = "3d_armor_inv_leggings_reinforcedleather.png",
 		groups = {armor_legs = 11, armor_heal = 0, armor_use = 40},
 		wear = 0,
 	})
 	-- Register boots :
 		minetest.register_tool(":3d_armor:boots_reinforcedleather", {
-		description = "Reinforced Leather Boots",
+		description = "Reinforced Leather Boots (Hunter)",
 		inventory_image = "3d_armor_inv_boots_reinforcedleather.png",
 		groups = {armor_feet = 6, armor_heal = 0, armor_use = 40},
 		wear = 0,
