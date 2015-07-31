8f8d9647fb3d71451406f72fdd167ed19baacab2
Ombridride <Ombridride@users.noreply.github.com>
2015-07-31 19:47:43 +0200

    Sometimes, strange things happenned

--- mods/3d_armor_classes/blackmithril/init.lua
+++ mods/3d_armor_classes/blackmithril/init.lua
@@ -38,40 +38,6 @@ minetest.register_craftitem(":3d_armor:blackmithril_ingot", {
 -- Black Mithril craft recipe
 minetest.register_craft({
 	output = "3d_armor:blackmithril_ingot",
-if ARMOR_MATERIALS.blackmithril then
-	-- Register helmets :
-	minetest.register_tool(":3d_armor:helmet_blackmithril", {
-		description = "Black Mithril Helmet (Warrior)",
-		inventory_image = "3d_armor_inv_helmet_blackmithril.png",
-		groups = {armor_head = 11, armor_heal = 0, armor_use = 40},
-		wear = 0,
-	})
-	-- Register chestplates :
-	minetest.register_tool(":3d_armor:chestplate_blackmithril", {
-		description = "Black Mithril Chestplate (Warrior)",
-		inventory_image = "3d_armor_inv_chestplate_blackmithril.png",
-		groups = {armor_torso = 17, armor_heal = 0, armor_use = 40},
-		wear = 0,
-	})
-	-- Register leggings :
-	minetest.register_tool(":3d_armor:leggings_blackmithril", {
-		description = "Black Mithril Leggings (Warrior)",
-		inventory_image = "3d_armor_inv_leggings_blackmithril.png",
-		groups = {armor_legs = 17, armor_heal = 0, armor_use = 40},
-		wear = 0,
-	})
-	-- Register boots :
-		minetest.register_tool(":3d_armor:boots_blackmithril", {
-		description = "Black Mithril Boots (Warrior)",
-		inventory_image = "3d_armor_inv_boots_blackmithril.png",
-		groups = {armor_feet = 11, armor_heal = 0, armor_use = 40},
-		wear = 0,
-	})
-end
-
--- Black Mithril craft recipe
-minetest.register_craft({
-	output = "3d_armor:blackmithril_ingot",
 	recipe = {
 		{"default:mithril_ingot","mobs:dungeon_master_blood"	,"default:obsidian"	},
 		{		      "", "mobs:dungeon_master_diamond"	, ""			},
