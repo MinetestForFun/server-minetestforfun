fa9e1e7a1e31487cee303c2f75118ce14a3f47c1
LeMagnesium <mg.minetest@gmail.com>
2015-08-27 19:41:34 +0200

    Add peace_areas mod  - Solves #240

---
+++ mods/peace_areas/init.lua
@@ -0,0 +1,47 @@
+--
+-- Peaceful areas
+-- Where hitting is impossible
+--
+
+peace_areas = {areas = {}}
+
+minetest.register_on_punchplayer(function(player, hitter)
+	local pos = player:getpos() 
+	for name, positions in pairs(peace_areas.areas) do
+		local pos1 = positions["pos1"]
+		local pos2 = positions["pos2"]
+		local minp = {
+			x = math.min(pos1.x, pos2.x),
+			y = math.min(pos1.y, pos2.y),
+			z = math.min(pos1.z, pos2.z)
+		}
+		local maxp = {
+			x = math.max(pos1.x, pos2.x),
+			y = math.max(pos1.y, pos2.y),
+			z = math.max(pos1.z, pos2.z)
+		}
+		if minp.x < pos.x and pos.x < maxp.x and
+			minp.y < pos.y and pos.y < maxp.y and
+			minp.z < pos.z and pos.z < maxp.z then
+			if hitter:is_player() then
+				minetest.chat_send_player(hitter:get_player_name(), "You cannot punch player " ..
+					player:get_player_name() .. ". They are in area " .. name .. ".")
+			end
+			return true
+			--[[
+			Note:
+				This use of the callback register_on_punchplayer may prevent other
+				instance of it to work correctly. Please take note of it.
+			]]--
+		end
+	end
+end)
+
+function peace_areas.register_house(area_name, def)
+	peace_areas.areas[area_name] = def
+end
+
+peace_areas.register_area("spawn", {
+	["pos1"] = {x = 16, y = 0, z = 12},
+	["pos2"] = {x = 42, y = 100, z = -14},
+})
--- worlds/minetestforfun/world.mt
+++ worlds/minetestforfun/world.mt
@@ -24,6 +24,7 @@ load_mod_automappercolors = true
 load_mod_mapfix = true
 load_mod_worldedge = true
 load_mod_maze = true
+load_mod_peace_areas = true
 
 load_mod_watershed = true
 load_mod_bushes_classic = true
