3baca43ef916cdf6dc52e9ff80e496a8188f048d
Gael-de-Sailly <gael.chretien@akeonet.com>
2015-08-28 13:46:32 +0200

    More mithril

--- mods/moreores/ores.lua
+++ mods/moreores/ores.lua
@@ -32,3 +32,36 @@ minetest.register_ore({
 	y_min     = -31000,
 	y_max     = -1024,
 })
+
+minetest.register_ore({
+	ore_type = "scatter",
+	ore = "moreores:mineral_mithril",
+	wherein = "default:stone",
+	clust_scarcity = 15 * 15 * 15,
+	clust_num_ores = 2,
+	clust_size     = 6,
+	y_min     = -31000,
+	y_max     = -2048,
+})
+
+minetest.register_ore({
+	ore_type = "scatter",
+	ore = "moreores:mineral_mithril",
+	wherein = "default:stone",
+	clust_scarcity = 22 * 22 * 22,
+	clust_num_ores = 5,
+	clust_size     = 9,
+	y_min     = -31000,
+	y_max     = -4096,
+})
+
+minetest.register_ore({
+	ore_type = "scatter",
+	ore = "moreores:mineral_mithril",
+	wherein = "default:stone",
+	clust_scarcity = 28 * 28 * 28,
+	clust_num_ores = 20,
+	clust_size     = 12,
+	y_min     = -31000,
+	y_max     = -12288,
+})
