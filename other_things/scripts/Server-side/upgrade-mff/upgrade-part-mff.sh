f53904be59efda44d8e9a55111d8ef318b0220d1
BUISSON-DEBON Quentin <contact@qbuissondebon.info>
2015-09-08 19:29:20 +0200

    reorganized the translation of the website and tweak scripts

--- other_things/scripts/Server-side/upgrade-mff/upgrade-part-mff.sh
+++ other_things/scripts/Server-side/upgrade-mff/upgrade-part-mff.sh
@@ -1,11 +1,11 @@
-# On récupère la dernière version du jeu
+# on récupère la dernière version du jeu
 cd /home/quentinbd/
 git clone https://github.com/MinetestForFun/server-minetestforfun.git
 echo "Clone de server-minetestforfun réussit."
 cd /home/quentinbd/server-minetestforfun/
 git submodule update --init --recursive
 
-# On sauvegarde les anciens ../games et ../mods
+# on sauvegarde les anciens minetest/games et minetest/mods
 rm -R /home/quentinbd/upgrade-mff/olds-part/games/
 rm -R /home/quentinbd/upgrade-mff/olds-part/mods/
 echo "Ancienne sauvegarde de /mods et /games correctement supprimée."
@@ -14,7 +14,7 @@ cp -R /home/quentinbd/mff/mods/ /home/quentinbd/upgrade-mff/olds-part/
 cp -R /home/quentinbd/mff/games/ /home/quentinbd/upgrade-mff/olds-part/
 echo "Sauvegarde de /mods et /games correctement effectuée."
 
-# On MAJ les nouveaux minetest/games et minetest/mods
+# on MAJ les nouveaux minetest/games et minetest/mods
 rm -R /home/quentinbd/mff/games/
 rm -R /home/quentinbd/mff/mods/
 mkdir /home/quentinbd/mff/games/
@@ -22,30 +22,26 @@ cp -R /home/quentinbd/server-minetestforfun/minetestforfun_game/ /home/quentinbd
 cp -R /home/quentinbd/server-minetestforfun/mods/ /home/quentinbd/mff/
 echo "Nouveaux /mods et /games correctement déplacés"
 
-# On MAJ le minetest.conf, world.mt, random_messages et le news.txt
-mkdir /home/quentinbd/mff/worlds/minetestforfun/
-rm /home/quentinbd/mff-hg/minetest.conf
-rm /home/quentinbd/mff/worlds/minetestforfun/world.mt
-rm /home/quentinbd/mff/worlds/minetestforfun/random_messages
+# on MAJ les news, random_messages et le world.mt
 rm /home/quentinbd/mff/worlds/minetestforfun/news.txt
-# On les remet
-cp /home/quentinbd/server-minetestforfun/minetest.conf /home/quentinbd/mff/
-cp /home/quentinbd/server-minetestforfun/worlds/minetestforfun/world.mt /home/quentinbd/mff/worlds/minetestforfun/
-cp /home/quentinbd/server-minetestforfun/worlds/minetestforfun/random_messages /home/quentinbd/mff/worlds/minetestforfun/
+rm /home/quentinbd/mff/worlds/minetestforfun/random_messages
+rm /home/quentinbd/mff/worlds/minetestforfun/world.mt
 cp /home/quentinbd/server-minetestforfun/worlds/minetestforfun/news.txt /home/quentinbd/mff/worlds/minetestforfun/
+cp /home/quentinbd/server-minetestforfun/worlds/minetestforfun/random_messages /home/quentinbd/mff/worlds/minetestforfun/
+cp /home/quentinbd/server-minetestforfun/worlds/minetestforfun/world.mt /home/quentinbd/mff/worlds/minetestforfun/
 echo "Nouvelles news.txt, world.mt  et random_messages correctement déplacé"
-
-# Suppression du dossier cloné
-rm -Rf /home/quentinbd/server-minetestforfun/
-echo "Bravo ! mff/mods et mff/games maintenant à jour"
 
 # TEMPORAIRE - ré-ajout de l'ancien mod irc
 rm -R /home/quentinbd/mff/mods/irc/
-cp -R /home/quentinbd/upgrade-mff/irc-old-save/ /home/quentinbd/mff/mods/
+cp -R /home/quentinbd/server-minetestforfun/other_things/irc-old-save/ /home/quentinbd/mff/mods/
 mv /home/quentinbd/mff/mods/irc-old-save/ /home/quentinbd/mff/mods/irc/
 echo "TEMPORAIRE - ré-ajout de l'ancien mod irc"
 
+# Suppression du dossier cloné
+rm -Rf /home/quentinbd/server-minetestforfun/
+echo "Bravo ! minetest/mods et minetest/games maintenant à jour"
+
 # On ré-attribut les droits à quentinbd et en 755
-chown -R quentinbd:quentinbd /home/quentinbd/mff/
-chmod -R 755 /home/quentinbd/mff/
+chown -R quentinbd:quentinbd /home/quentinbd/
+chmod -R 755 /home/quentinbd/
 echo "ré-attribution des droits à quentinbd:quentinbd"
