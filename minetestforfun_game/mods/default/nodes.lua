81f82bad1578c84eca8ea40619d06d11f003fde2
LeMagnesium <mg.minetest@gmail.com>
2015-09-15 22:24:56 +0200

    Update MinetestForFunGame

--- minetestforfun_game/mods/default/nodes.lua
+++ minetestforfun_game/mods/default/nodes.lua
@@ -2232,7 +2232,8 @@ minetest.register_node("default:glass", {
 minetest.register_node("default:obsidian_glass", {
 	description = "Obsidian Glass",
 	drawtype = "glasslike_framed_optional",
-	tiles = {"default_obsidian_glass_frame.png", "default_obsidian_glass_detail.png"},
+	tiles = {"default_obsidian_glass.png", "default_obsidian_glass_detail.png"},
+	inventory_image = minetest.inventorycube("default_obsidian_glass.png"),
 	paramtype = "light",
 	is_ground_content = false,
 	sunlight_propagates = true,
