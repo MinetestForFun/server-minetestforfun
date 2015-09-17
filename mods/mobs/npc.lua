4494702a9b831cfe9a408a82dea00e968e8e9f04
Ombridride <Ombridride@users.noreply.github.com>
2015-09-17 23:06:41 +0200

    3 times less NPC male

--- mods/mobs/npc.lua
+++ mods/mobs/npc.lua
@@ -118,6 +118,6 @@ mobs:register_mob("mobs:npc", {
 })
 
 -- spawning enable for now
-mobs:spawn_specific("mobs:npc", {"default:dirt_with_grass", "default:dirt", "default:junglegrass", "default:sand"}, {"air"}, -1, 20, 30, 100000, 1, -31000, 31000, true)
+mobs:spawn_specific("mobs:npc", {"default:dirt_with_grass", "default:dirt", "default:junglegrass", "default:sand"}, {"air"}, -1, 20, 30, 300000, 1, -31000, 31000, true)
 -- register spawn egg
 mobs:register_egg("mobs:npc", "Npc", "mobs_npc_male_inv.png", 1)
