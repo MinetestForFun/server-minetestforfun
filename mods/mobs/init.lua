7dea0a468aab585daa51a86cc39092b1c7383ca2
LeMagnesium <mg.minetest@gmail.com>
2015-09-25 21:46:02 +0200

    Revert "[mobs] Add jellyfish"
    This reverts commit 33d305d2686b0a3c01a48f08cd48d17f5bb45cc0.

--- mods/mobs/init.lua
+++ mods/mobs/init.lua
@@ -15,10 +15,7 @@ dofile(path.."/bee.lua") -- KrupnoPavel
 dofile(path.."/bunny.lua") -- ExeterDad
 dofile(path.."/kitten.lua") -- Jordach/BFD
 dofile(path.."/goat.lua") -- ???
-
--- Fish
 dofile(path.."/shark.lua") -- blert2112 (animal_modpack)
-dofile(path.."/jellyfish.lua") -- blert2112
 
 -- Monsters
 
