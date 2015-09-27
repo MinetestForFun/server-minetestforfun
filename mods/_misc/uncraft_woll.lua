1e83c2a28fbc63aaafa15b46e87163dbe48175b2
LeMagnesium <mg.minetest@gmail.com>
2015-09-27 20:30:22 +0200

    [_misc] Fix infinite wool craft  - See https://www.youtube.com/watch?v=hhAq9mHZprk

--- mods/_misc/uncraft_woll.lua
+++ mods/_misc/uncraft_woll.lua
@@ -1,118 +1,118 @@
 -- récupéré les "cotton" des "wool" faite (récupère 3 aulieu de 4 et perte du colorant)
 minetest.register_craft({
-	output = "farming:cotton 9",
+	output = "farming:cotton 4",
 	recipe = {
 		{"wool:black"},
 	},
 })
 
 minetest.register_craft({
-	output = "farming:cotton 9",
+	output = "farming:cotton 4",
 	recipe = {
 		{"wool:white"},
 	},
 })
 
 minetest.register_craft({
-	output = "farming:cotton 9",
+	output = "farming:cotton 4",
 	recipe = {
 		{"wool:grey"},
 	},
 })
 
 minetest.register_craft({
-	output = "farming:cotton 9",
+	output = "farming:cotton 4",
 	recipe = {
 		{"wool:dark_grey"},
 	},
 })
 
 minetest.register_craft({
-	output = "farming:cotton 9",
+	output = "farming:cotton 4",
 	recipe = {
 		{"wool:blue"},
 	},
 })
 
 minetest.register_craft({
-	output = "farming:cotton 9",
+	output = "farming:cotton 4",
 	recipe = {
 		{"wool:brown"},
 	},
 })
 
 minetest.register_craft({
-	output = "farming:cotton 9",
+	output = "farming:cotton 4",
 	recipe = {
 		{"wool:cyan"},
 	},
 })
 
 minetest.register_craft({
-	output = "farming:cotton 9",
+	output = "farming:cotton 4",
 	recipe = {
 		{"wool:green"},
 	},
 })
 
 minetest.register_craft({
-	output = "farming:cotton 9",
+	output = "farming:cotton 4",
 	recipe = {
 		{"wool:dark_green"},
 	},
 })
 
 minetest.register_craft({
-	output = "farming:cotton 9",
+	output = "farming:cotton 4",
 	recipe = {
 		{"wool:dark_grey"},
 	},
 })
 
 minetest.register_craft({
-	output = "farming:cotton 9",
+	output = "farming:cotton 4",
 	recipe = {
 		{"wool:dark_grey"},
 	},
 })
 
 minetest.register_craft({
-	output = "farming:cotton 9",
+	output = "farming:cotton 4",
 	recipe = {
 		{"wool:magenta"},
 	},
 })
 
 minetest.register_craft({
-	output = "farming:cotton 9",
+	output = "farming:cotton 4",
 	recipe = {
 		{"wool:orange"},
 	},
 })
 
 minetest.register_craft({
-	output = "farming:cotton 9",
+	output = "farming:cotton 4",
 	recipe = {
 		{"wool:pink"},
 	},
 })
 
 minetest.register_craft({
-	output = "farming:cotton 9",
+	output = "farming:cotton 4",
 	recipe = {
 		{"wool:red"},
 	},
 })
 
 minetest.register_craft({
-	output = "farming:cotton 9",
+	output = "farming:cotton 4",
 	recipe = {
 		{"wool:violet"},
 	},
 })
 
 minetest.register_craft({
-	output = "farming:cotton 9",
+	output = "farming:cotton 4",
 	recipe = {
 		{"wool:yellow"},
 	},
