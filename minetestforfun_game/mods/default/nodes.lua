9a3a835771c78b78f9e6a29848728997f2b1cc68
LeMagnesium <mg.minetest@gmail.com>
2015-10-07 14:02:36 +0200

    [mesecons] Update mesecons - Also fix whitespaces in default/nodes.lua

--- minetestforfun_game/mods/default/nodes.lua
+++ minetestforfun_game/mods/default/nodes.lua
@@ -563,7 +563,7 @@ minetest.register_node("default:sapling", {
 		type = "fixed",
 		fixed = {-0.3, -0.5, -0.3, 0.3, 0.35, 0.3}
 	},
-	groups = {snappy = 3, flammable=2, 
+	groups = {snappy = 3, flammable=2,
 		attached_node = 1, sapling = 1},
 	sounds = default.node_sound_leaves_defaults(),
 })
