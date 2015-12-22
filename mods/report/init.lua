b060e77dbe01ce643a16bb3ac155725312d0abc6
LeMagnesium <mg.minetest@gmail.com>
2015-12-22 16:47:39 +0100

    [report] Add a random message, and a button
    - Part two and three of #340

--- mods/report/init.lua
+++ mods/report/init.lua
@@ -34,3 +34,49 @@ minetest.register_chatcommand("report", {
 		end
 	end
 })
+
+if minetest.get_modpath("unified_inventory") then
+	unified_inventory.register_button("report", {
+		type = "image",
+		-- From http://www.clker.com/cliparts/v/K/Y/P/2/M/warning-sign-bl-bg-hi.png
+		image = "report_button.png",
+		tooltip = "Report to the moderators/administrator",
+	})
+
+	unified_inventory.register_page("report", {
+		get_formspec = function(player)
+			local form = "field[2,4;5,1;text;Text about what to report:;]" ..
+				"button[2,5;2,0.5;report;Send]"
+			return {formspec = form, draw_inventory = false}
+		end
+	})
+
+	minetest.register_on_player_receive_fields(function(player, formname, fields)
+		if formname ~= "" or not fields.report then
+			return
+		end
+
+		-- Copied from src/builtin/game/chatcommands.lua (with little tweaks)
+		if not fields.text or fields.text == "" then
+			return
+		end
+		local name = player:get_player_name()
+		local cmd_def = core.chatcommands["report"]
+		if not cmd_def then
+			return
+		end
+		local has_privs, missing_privs = core.check_player_privs(name, cmd_def.privs)
+		if has_privs then
+			core.set_last_run_mod(cmd_def.mod_origin)
+			local success, message = cmd_def.func(name, fields.text)
+			if message then
+				core.chat_send_player(name, message)
+			end
+		else
+			core.chat_send_player(name, "You don't have permission"
+					.. " to run this command (missing privileges: "
+					.. table.concat(missing_privs, ", ") .. ")")
+		end
+		return true -- Handled fields reception
+	end)
+end
