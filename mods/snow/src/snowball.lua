8322577bea2f672369a4c70fbf35d2578e7ab2ff
LeMagnesium <mg.minetest@gmail.com>
2015-12-29 16:21:49 +0100

    [snow] Prevent snowballs from colliding against each other

--- mods/snow/src/snowball.lua
+++ mods/snow/src/snowball.lua
@@ -118,8 +118,8 @@ function snow_snowball_ENTITY.on_step(self, dtime)
 
 	if self.timer > 0.15 then
 		for i, v in pairs(minetest.get_objects_inside_radius(self.object:getpos(), 1)) do
-			if v ~= self.object and v:get_entity_name() ~= "__builtin:item"
-				and v:get_entity_name() ~= "gauges:hp_bar" then
+			if v:get_entity_name() ~= "snow:snowball_entity" and v:get_entity_name() ~= "__builtin:item"
+				and v:get_entity_name() ~= "gauges:hp_bar") then
 				v:punch(minetest.get_player_by_name(self.thrower), 1.0,{full_punch_interval=1.0, damage_groups = {fleshy=1} })
 				minetest.add_item(self.object:getpos(), "default:snow")
 				self.object:remove()
