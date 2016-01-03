7621b867c3f7b4f8125545703eea1cfd6d4b6986
LeMagnesium <mg.minetest@gmail.com>
2016-01-03 16:02:42 +0100

    [mobs] Replace warthogs by pigs
    - For #279

--- mods/mobs/init.lua
+++ mods/mobs/init.lua
@@ -10,13 +10,12 @@ dofile(path.."/chicken.lua") -- JKmurray
 dofile(path.."/cow.lua") -- KrupnoPavel
 dofile(path.."/rat.lua") -- PilzAdam
 dofile(path.."/sheep.lua") -- PilzAdam
-dofile(path.."/warthog.lua") -- KrupnoPavel
+dofile(path.."/pig.lua") -- farfadet46
 dofile(path.."/bee.lua") -- KrupnoPavel
 dofile(path.."/bunny.lua") -- ExeterDad
 dofile(path.."/kitten.lua") -- Jordach/BFD
 dofile(path.."/goat.lua") -- ???
 dofile(path.."/shark.lua") -- blert2112 (animal_modpack)
-dofile(path.."/pig.lua") -- farfadet46
 
 -- Monsters
 
--- mods/mobs/pig.lua
+++ mods/mobs/pig.lua
@@ -8,6 +8,7 @@ mobs:register_mob("mobs:pig", {
 	passive = false,
 	group_attack = true,
 	attack_type = "dogfight",
+	reach = 2,
 	damage = 4,
 	-- health & armor
 	hp_min = 15,
@@ -21,28 +22,33 @@ mobs:register_mob("mobs:pig", {
 	textures = {
 		{"mobs_pig_pink.png"},
 	},
-	blood_texture = "mobs_blood.png",
 	visual_size = {x=1,y=1},
+	blood_texture = "mobs_blood.png",
 	-- sounds
 	makes_footstep_sound = true,
 	sounds = {
---		random = "mobs_pig", We don't have that yet
+		random = "mobs_pig",
+		attack = "mobs_pig_angry",
 	},
 	-- speed and jump
-	walk_velocity = 1.5,
+	walk_velocity = 2,
 	run_velocity = 3,
-	jump = false,
-	-- drops raw meat when dead
+	jump = true,
+	-- follows apple and potato
+	follow = {"default:apple", "farming:potato"},
+	view_range = 10,
+	-- drops raw pork when dead
 	drops = {
-		{name = "mobs:meat_raw",
-		chance = 1, min = 2, max = 4},
-		{name = "maptools:copper_coin",
+		{name = "mobs:pork_raw",
+		chance = 1, min = 2, max = 3,},
+		{name = "maptools:silver_coin",
 		chance = 10, min = 1, max = 1,},
 	},
 	-- damaged by
 	water_damage = 1,
 	lava_damage = 5,
 	light_damage = 0,
+	fear_height = 2,
 	-- model animation
 	animation = {
 		speed_normal = 15,
@@ -50,9 +56,34 @@ mobs:register_mob("mobs:pig", {
 		walk_start = 60,		walk_end = 100,
 		punch_start = 60,		punch_end = 100,
 	},
-	view_range = 10,
+	-- can be tamed by feeding 8 wheat (will not attack when tamed)
+	on_rightclick = function(self, clicker)
+		mobs:feed_tame(self, clicker, 8, true, true)
+		mobs:capture_mob(self, clicker, 0, 5, 50, false, nil)
+	end,
 })
--- spawn on dirt_with_grass between -1 and 20 light, 1 in 20000 chance, 1 goat in area up to 31000 in height
-mobs:spawn_specific("mobs:pig", {"default:dirt_with_grass"}, {"air"}, -1, 20, 30, 10000, 1, -31000, 31000, true)
+-- spawns on dirt or junglegrass, between 8 and 20 light, 1 in 15000 chance, 1 in area up to 31000 in height
+mobs:spawn_specific("mobs:pig", {"default:dirt", "default:junglegrass", "default:dirt_with_dry_grass"}, {"air"}, 8, 20, 30, 10000, 1, -31000, 31000, true)
 -- register spawn egg
 mobs:register_egg("mobs:pig", "Pig", "mobs_pig_inv.png", 1)
+
+-- porkchop (raw and cooked)
+minetest.register_craftitem("mobs:pork_raw", {
+	description = "Raw Porkchop",
+	inventory_image = "mobs_pork_raw.png",
+	on_use = minetest.item_eat(4),
+})
+
+-- cooked porkchop
+minetest.register_craftitem("mobs:pork_cooked", {
+	description = "Cooked Porkchop",
+	inventory_image = "mobs_pork_cooked.png",
+	on_use = minetest.item_eat(8),
+})
+
+minetest.register_craft({
+	type = "cooking",
+	output = "mobs:pork_cooked",
+	recipe = "mobs:pork_raw",
+	cooktime = 5,
+})
