9c0abf065c9365888fb9212fb40f64346c1fdd8c
LeMagnesium <mg.minetest@gmail.com>
2016-01-31 15:22:29 +0100

    [3dmushrooms] Redirect aliases to the new itemstrings
    - Fix #390

--- mods/plantlife_modpack/3dmushrooms/init.lua
+++ mods/plantlife_modpack/3dmushrooms/init.lua
@@ -5,17 +5,6 @@
 
 mushroom = {}
 
-minetest.override_item("flowers:mushroom_fertile_brown", {
-	drawtype = "mesh",
-	mesh = "3dmushrooms.obj",
-	tiles = {"3dmushrooms_brown.png"},
-	selection_box = {
-		type = "fixed",
-		fixed = {-0.3, -0.5, -0.3, 0.3, 0, 0.3}
-	},
-	inventory_image = "3dmushrooms_brown_inv.png"
-})
-
 minetest.override_item("flowers:mushroom_brown", {
 	drawtype = "mesh",
 	mesh = "3dmushrooms.obj",
@@ -25,17 +14,6 @@ minetest.override_item("flowers:mushroom_brown", {
 		fixed = {-0.3, -0.5, -0.3, 0.3, 0, 0.3}
 	},
 	inventory_image = "3dmushrooms_brown_inv.png"
-})
-
-minetest.override_item("flowers:mushroom_fertile_red", {
-	drawtype = "mesh",
-	mesh = "3dmushrooms.obj",
-	tiles = {"3dmushrooms_red.png"},
-	selection_box = {
-		type = "fixed",
-		fixed = {-0.3, -0.5, -0.3, 0.3, 0, 0.3}
-	},
-	inventory_image = "3dmushrooms_red_inv.png"
 })
 
 minetest.override_item("flowers:mushroom_red", {
@@ -52,15 +30,15 @@ minetest.override_item("flowers:mushroom_red", {
 -- aliases to the default mushrooms
 
 minetest.register_alias("mushroom:brown", "flowers:mushroom_brown")
-minetest.register_alias("mushroom:brown_natural", "flowers:mushroom_fertile_brown")
-minetest.register_alias("mushroom:spore_brown", "flowers:mushroom_spores_brown")
-minetest.register_alias("mushroom:spore2", "flowers:mushroom_spores_brown")
+minetest.register_alias("mushroom:brown_natural", "flowers:mushroom_brown")
+minetest.register_alias("mushroom:spore_brown", "flowers:mushroom_brown")
+minetest.register_alias("mushroom:spore2", "flowers:mushroom_brown")
 minetest.register_alias("mushroom:brown_essence", "flowers:mushroom_brown")
 
 minetest.register_alias("mushroom:red", "flowers:mushroom_red")
-minetest.register_alias("mushroom:red_natural", "flowers:mushroom_fertile_red")
-minetest.register_alias("mushroom:spore_red", "flowers:mushroom_spores_red")
-minetest.register_alias("mushroom:spore1", "flowers:mushroom_spores_red")
+minetest.register_alias("mushroom:red_natural", "flowers:mushroom_red")
+minetest.register_alias("mushroom:spore_red", "flowers:mushroom_red")
+minetest.register_alias("mushroom:spore1", "flowers:mushroom_red")
 minetest.register_alias("mushroom:poison", "flowers:mushroom_red")
 
 minetest.register_alias("mushroom:identifier", "default:mese_crystal_fragment")
