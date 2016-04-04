f25082ad5fefdfc1e5f53bbefd026cd62c29136b
LeMagnesium <mg.minetest@gmail.com>
2016-04-04 19:29:51 +0200

    [snow] Implement alias between snow and default pine sapling
     - Maybe solve #425

--- mods/snow/aliases.lua
+++ mods/snow/aliases.lua
@@ -1,2 +1,3 @@
 minetest.register_alias("default:pine_needles", "snow:needles")
 minetest.register_alias("default:pine_needles", "snow:leaves")
+minetest.register_alias("default:pine_sapling", "snow:sapling_pine")
