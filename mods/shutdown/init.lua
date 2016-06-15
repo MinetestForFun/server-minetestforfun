6387a54bb767ff947bc7fcde7c7f6fd6908bbe01
LeMagnesium <mg.minetest@gmail.com>
2016-06-15 03:52:35 +0200

    [shutdown] Fix hour detection and tick more often

--- mods/shutdown/init.lua
+++ mods/shutdown/init.lua
@@ -32,7 +32,7 @@ local function tick()
 	local heure = os.date("%H")
 	local minute = os.date("%M")
 	-- Warn every days
-	if heure == "4" then
+	if heure == "04" then
 		if minute == "00" then
 			minetest.chat_send_all("Rappel : Redémarrage journalier du serveur dans 30 minutes. (Dure 30 minutes)")
 			minetest.chat_send_all("Reminder : Daily reboot of the server in 30 minutes. (Lasts 30 minutes)")
@@ -52,7 +52,7 @@ local function tick()
 	--		minetest.request_shutdown()
 		end
 	end
-	minetest.after(60, tick)
+	minetest.after(20, tick)
 end
 
 tick()
