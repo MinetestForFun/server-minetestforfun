6f5b2d4890547b235ae13da0f5cee9f456eec5aa
LeMagnesium <mg.minetest@gmail.com>
2016-06-20 23:51:01 +0200

    [snow] Reverse ineffective aliases to make them work; for #425

--- mods/snow/aliases.lua
+++ mods/snow/aliases.lua
@@ -1,3 +1,3 @@
-minetest.register_alias("default:pine_needles", "snow:needles")
-minetest.register_alias("default:pine_needles", "snow:leaves")
-minetest.register_alias("default:pine_sapling", "snow:sapling_pine")
+minetest.register_alias("snow:needles", "default:pine_needles")
+minetest.register_alias("snow:leaves", "default:pine_needles")
+minetest.register_alias("snow:sapling_pine", "default:pine_sapling")
