e13fb9949483e83a79e495cd6081e4d5198f6741
ObaniGemini <robin@pi-et-ro.net>
2017-07-23 19:29:20 +0200

    Remove other_things (moved to another repo) This is a step to reorganization

--- other_things/logos/README.md
+++
@@ -1,3 +0,0 @@
-Remarque : 
-
-Ajouter d'autres logos avec des cubes ou dessins à l'intérieur serait bien pour la diversité du sondage/vote futur ! :)
--- other_things/scripts/Server-side/script/news-mff-rsync.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh 
-#rsync -e "ssh -i /root/.ssh/id_dsa" -av --delete-after /home/quentinbd/minetest/worlds/minetestforfun/news.txt root@192.168.1.20:/var/www/wordpress/wp-content/uploads/news/
-# Check the public key rights
-chmod 600 /home/quentinbd/.ssh/id_rsa
-chmod 600 /home/quentinbd/.ssh/id_rsa.pub
-# Begin the RSYNC
-rsync -azrv --delete /home/quentinbd/mff/worlds/minetestforfun/news.txt quentinbd@192.168.1.20:/var/www/wordpress/wp-content/uploads/news/
-echo "Transfert réussi de news.txt sur le wordpress"
